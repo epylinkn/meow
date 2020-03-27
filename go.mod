fc5ba0002a4edfd85b0ce0bc0e74cd9503f4584a
Anthony Bui <anthonybui@gmail.com>
2020-03-26 21:33:34 -0700

    update brew formula

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/epylinkn/meow
 go 1.14
 
 require (
-	github.com/gookit/color v1.2.3 // indirect
+	github.com/gookit/color v1.2.3
 	github.com/urfave/cli v1.22.3
 )
--- pkg/brew/meow-bin.rb
+++ pkg/brew/meow-bin.rb
@@ -1,8 +1,8 @@
 class MeowBin < Formula
   desc "Day previewer written in Golang"
   homepage "https://github.com/epylinkn/meow"
-  url ''
-  sha256 ''
+  url 'https://github.com/epylinkn/meow/releases/download/v0.1.0/meow-bin.tar.gz'
+  sha256 '3dbbb512f2d9a82a706b6be471e13c8922a53c3d29d0a7627225667f1629c8ed'
   version '0.1.0'
 
   depends_on "ical_buddy"
