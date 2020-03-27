96f635e3f7759fe9f1b7f2b342fc0500ca3f55d3
Anthony Bui <anthonybui@gmail.com>
2020-03-26 21:36:43 -0700

    Fix brew dependency

--- pkg/brew/meow-bin.rb
+++ pkg/brew/meow-bin.rb
@@ -5,7 +5,7 @@ class MeowBin < Formula
   sha256 '3dbbb512f2d9a82a706b6be471e13c8922a53c3d29d0a7627225667f1629c8ed'
   version '0.1.0'
 
-  depends_on "ical_buddy"
+  depends_on "ical-buddy"
 
   def install
     bin.install "meow"
